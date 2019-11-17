e0c87b4e3912809242964bf4b43a88787ea1e772
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-17 13:26:01 -0300

    Hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['reactJS', 'react Native']);
+  const [newTech, setNewTech] = useState('');
+
+  /**
+   * copia todas as infos. do tech "...tech", pois o estado continua imutavel, por isso tem q copiar
+   * e adc uma nova info no final, "newTech" para servir de input
+   */
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;

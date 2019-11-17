7f6864908f6f58701cf5940cc1517053d9bdd6fc
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-17 15:52:37 -0300

    Hook useCallback

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
-/**
- * useState - Pertence a função, cria estados na função sem pertencer
- * ao formato de classes. P/ cada tipo de info. dentro do componente,
- * teremos um estado separado
- *
- */
 function App() {
   const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
-
-  /**
-   * copia todas as infos. do tech "...tech", pois o estado continua imutavel, por isso tem q copiar
-   * e adc uma nova info no final, "newTech" para servir de input
-   * setNewTech reseta valor do input assim que adiciona
-   */
-  function handleAdd() {
+  const handleAdd = useCallback(() => {
     setTech([...tech, newTech]);
     setNewTech('');
-  }
-  /**
-   * executa apenas uma vez com passagem do segundo parametro no array
-   * de dependencia vazio, deixa salvo os dados
-   */
+  }, [newTech, tech]);
+
   useEffect(() => {
     const storageTech = localStorage.getItem('tech');
 
@@ -31,11 +16,6 @@ function App() {
     }
   }, []);
 
-  /**
-   * substitui o didMount, o primeiro parametro receabe a funcao que será executado
-   * e o segundo é apartir dele que será executado a primeira, ou seja,
-   * qualquer açao que o segundo parametro fizer, execute a primeira
-   */
   useEffect(() => {
     localStorage.setItem('tech', JSON.stringify(tech));
   }, [tech]);
